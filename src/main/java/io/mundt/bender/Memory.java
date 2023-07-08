d722a989f5157f2f4d0708c90a4465083c48f500
Michele Delle Vergini <michele.dellevergini@outlook.com>
2023-07-08 19:56:29 +0100

    Avoid extra fun call for address upscaling

--- src/main/java/io/mundt/bender/Memory.java
+++ src/main/java/io/mundt/bender/Memory.java
@@ -4,29 +4,23 @@ public class Memory {
     private final byte[] data = new byte[64 * 1024];
 
     public byte readByte(short address) {
-        return data[upscale(address)];
+        return data[address & 0xFFFF];
     }
 
     public void writeByte(short address, byte value) {
-        data[upscale(address)] = value;
+        data[address & 0xFFFF] = value;
     }
 
     public short readWord(short address) {
-        int upscaledAddress = upscale(address);
-        int low = data[upscaledAddress] & 0xFF;
-        int high = data[upscaledAddress + 1] & 0xFF;
+        int low = data[address & 0xFFFF] & 0xFF;
+        int high = data[(address & 0xFFFF) + 1] & 0xFF;
         return (short) ((high << 8) | low);
     }
 
     public void writeWord(short address, short value) {
-        int upscaledAddress = upscale(address);
         int low = value & 0xFF;
         int high = (value >> 8) & 0xFF;
-        data[upscaledAddress] = (byte) low;
-        data[upscaledAddress + 1] = (byte) high;
-    }
-
-    private int upscale(short address) {
-        return address & 0xFFFF;
+        data[address & 0xFFFF] = (byte) low;
+        data[(address & 0xFFFF) + 1] = (byte) high;
     }
 }
