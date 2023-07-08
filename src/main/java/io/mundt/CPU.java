f981f4931659054cf5731a0439f24d0b1240da39
Michele Delle Vergini <michele.dellevergini@outlook.com>
2023-07-08 02:56:05 +0100

    Add LDA opcode

--- src/main/java/io/mundt/CPU.java
+++ src/main/java/io/mundt/CPU.java
@@ -29,6 +29,75 @@ public class CPU {
     public int step() {
         byte opcode = fetchByte();
         switch (opcode) {
+            case (byte) 0xA9 -> { // LDA #nn
+                a = fetchByte();
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                return 2;
+            }
+            case (byte) 0xA5 -> { // LDA nn
+                a = memory.readByte(fetchByte());
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                return 3;
+            }
+            case (byte) 0xB5 -> { // LDA nn,X
+                a = memory.readByte((short) (fetchByte() + x));
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                return 4;
+            }
+            case (byte) 0xAD -> { // LDA nnnn
+                a = memory.readByte(fetchWord());
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                return 4;
+            }
+            case (byte) 0xBD -> { // LDA nnnn,X
+                short absoluteAddress = fetchWord();
+                short effectiveAddress = (short) (absoluteAddress + x);
+                a = memory.readByte(effectiveAddress);
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                if ((effectiveAddress & 0xFF00) != (absoluteAddress & 0xFF00)) {
+                    return 5;
+                } else {
+                    return 4;
+                }
+            }
+            case (byte) 0xB9 -> { // LDA nnnn,Y
+                short absoluteAddress = fetchWord();
+                short effectiveAddress = (short) (absoluteAddress + y);
+                a = memory.readByte(effectiveAddress);
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                if ((effectiveAddress & 0xFF00) != (absoluteAddress & 0xFF00)) {
+                    return 5;
+                } else {
+                    return 4;
+                }
+            }
+            case (byte) 0xA1 -> { // LDA (nn,X)
+                short indirectAddress = (short) (fetchByte() + x);
+                short effectiveAddress = memory.readWord(indirectAddress);
+                a = memory.readByte(effectiveAddress);
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                return 6;
+            }
+            case (byte) 0xB1 -> { // LDA (nn),Y
+                short indirectAddress = fetchByte();
+                short absoluteAddress = memory.readWord(indirectAddress);
+                short effectiveAddress = (short) (absoluteAddress + y);
+                a = memory.readByte(effectiveAddress);
+                zeroFlag = a == 0;
+                negativeFlag = (a & 0x80) != 0;
+                if ((effectiveAddress & 0xFF00) != (absoluteAddress & 0xFF00)) {
+                    return 6;
+                } else {
+                    return 5;
+                }
+            }
             default -> throw new RuntimeException("Unknown opcode: " + opcode);
         }
     }
--- src/test/java/io/mundt/CPUTest.java
+++ src/test/java/io/mundt/CPUTest.java
@@ -49,4 +49,168 @@ public class CPUTest extends TestCase {
             assertEquals("Unknown opcode: 0", e.getMessage());
         }
     }
+
+    public void testLDAImmediate() {
+        memory.writeByte((short) 0x1234, (byte) 0xA9); // LDA #nn
+        memory.writeByte((short) 0x1235, (byte) 0x42); // #nn = 0x42
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals(0x42, cpu.a); // A = #nn
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertFalse(cpu.negativeFlag); // N = false
+        assertEquals(2, cycles); // 2 cycles
+    }
+
+    public void testLDAZeroPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xA5); // LDA nn
+        memory.writeByte((short) 0x1235, (byte) 0x42); // nn = 0x42
+        memory.writeByte((short) 0x0042, (byte) 0x84); // 0x42 = 0x84
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nn]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(3, cycles); // 3 cycles
+    }
+
+    public void testLDAZeroPageX() {
+        memory.writeByte((short) 0x1234, (byte) 0xB5); // LDA nn,X
+        memory.writeByte((short) 0x1235, (byte) 0x42); // nn = 0x42
+        memory.writeByte((short) 0x0052, (byte) 0x84); // 0x42 = 0x84
+        cpu.x = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nn + X]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(4, cycles); // 4 cycles
+    }
+
+    public void testLDAAbsolute() {
+        memory.writeByte((short) 0x1234, (byte) 0xAD); // LDA nnnn
+        memory.writeWord((short) 0x1235, (short) 0x5678); // nnnn = 0x5678
+        memory.writeByte((short) 0x5678, (byte) 0x84); // 0x5678 = 0x84
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nnnn]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(4, cycles); // 4 cycles
+    }
+
+    public void testLDAAbsoluteXWithinPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xBD); // LDA nnnn,X
+        memory.writeWord((short) 0x1235, (short) 0x5678); // nnnn = 0x5678
+        memory.writeByte((short) 0x5688, (byte) 0x84); // 0x5678 = 0x84
+        cpu.x = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nnnn + X]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(4, cycles); // 4 cycles
+    }
+
+    public void testLDAAbsoluteXCrossingPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xBD); // LDA nnnn,X
+        memory.writeWord((short) 0x1235, (short) 0x56F0); // nnnn = 0x56F0
+        memory.writeByte((short) 0x5700, (byte) 0x84); // 0x5700 = 0x84
+        cpu.x = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nnnn + X]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(5, cycles); // 5 cycles
+    }
+
+    public void testLDAAbsoluteYWithinPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xB9); // LDA nnnn,Y
+        memory.writeWord((short) 0x1235, (short) 0x5678); // nnnn = 0x5678
+        memory.writeByte((short) 0x5688, (byte) 0x84); // 0x5678 = 0x84
+        cpu.y = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nnnn + Y]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(4, cycles); // 4 cycles
+    }
+
+    public void testLDAAbsoluteYCrossingPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xB9); // LDA nnnn,Y
+        memory.writeWord((short) 0x1235, (short) 0x56F0); // nnnn = 0x56F0
+        memory.writeByte((short) 0x5700, (byte) 0x84); // 0x5700 = 0x84
+        cpu.y = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [nnnn + Y]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(5, cycles); // 5 cycles
+    }
+
+    public void testLDAIndirectX() {
+        memory.writeByte((short) 0x1234, (byte) 0xA1); // LDA (nn,X)
+        memory.writeByte((short) 0x1235, (byte) 0x42); // nn = 0x42
+        memory.writeWord((short) 0x0052, (short) 0x5678); // 0x52 = 0x5678
+        memory.writeByte((short) 0x5678, (byte) 0x84); // 0x5678 = 0x84
+        cpu.x = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [[nn + X]]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(6, cycles); // 6 cycles
+    }
+
+    public void testLDAIndirectYWithinPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xB1); // LDA (nn),Y
+        memory.writeByte((short) 0x1235, (byte) 0x42); // nn = 0x42
+        memory.writeWord((short) 0x0042, (short) 0x5678); // 0x42 = 0x5678
+        memory.writeByte((short) 0x5688, (byte) 0x84); // 0x5678 = 0x84
+        cpu.y = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [[nn] + Y]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(5, cycles); // 5 cycles
+    }
+
+    public void testLDAIndirectYCrossingPage() {
+        memory.writeByte((short) 0x1234, (byte) 0xB1); // LDA (nn),Y
+        memory.writeByte((short) 0x1235, (byte) 0x42); // nn = 0x42
+        memory.writeWord((short) 0x0042, (short) 0x56F0); // 0x42 = 0x56F0
+        memory.writeByte((short) 0x5700, (byte) 0x84); // 0x5700 = 0x84
+        cpu.y = 0x10;
+
+        cpu.pc = (short) 0x1234;
+        int cycles = cpu.step();
+
+        assertEquals((byte) 0x84, cpu.a); // A = [[nn] + Y]
+        assertFalse(cpu.zeroFlag); // Z = false
+        assertTrue(cpu.negativeFlag); // N = true
+        assertEquals(6, cycles); // 6 cycles
+    }
 }
